d933e092b479e3da512fff95d0ed56d427974564
adncode <deepartesdigitales@gmail.com>
2021-01-04 19:28:40 -0500

    Add files via upload

---
+++ js/post.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        posts: [
+            // entrada inicio //
+            {
+                link: 'post/post1.html',
+                nombre: 'Titulo entrada 1',
+                picture: 'img/img4.png',
+                descripcion: 'Esta es la descripcion de la primera entrada'
+            },
+            // entrada final //
+        ]
+    }
+})
